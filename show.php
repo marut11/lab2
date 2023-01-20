0296dc71ed6d8a73b4616729eb65f625a7535a18
marut11 <122139648+marut11@users.noreply.github.com>
2023-01-20 22:05:44 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,5 +1,20 @@
 <?php
-    $name=$_POST["name"];
-    $email=$_POST["email"];
-    echo "Name=$name, Email=$email";
+$n=date("N"); 
+$name=$_POST["name"];
+$old=$_POST["old"];
+$sex=$_POST["Sex"];
+$S=$_POST["S"];
+
+if($old <= "15"&& $sex =="ชาย"){
+    $n=("ดช.");
+}elseif($old >"15"&& $sex =="ชาย"){
+    $n=("นาย.");
+}elseif($old <="15"&& $sex =="หญิง"){
+    $n=("ดญ.");
+}elseif($old >"15"&& $sex =="หญิง" && $S == "แต่งงานแล้ว"){
+    $n=("นาง.");
+}elseif($old >"15"&& $sex =="หญิง"){
+    $n=("นางสาว.");
+}
+echo"ชื่อ:$n$name สถานะ:$S อายุ:$old " ;    
 ?>
