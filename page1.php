ab35fcdb5439b18e62920a8efcc04f2d534e381c
marut11 <122139648+marut11@users.noreply.github.com>
2023-01-20 22:00:27 +0700

    show.php
    <?php
    $n=date("N"); 
    $name=$_POST["name"];
    $email=$_POST["email"];
    echo"Name=$name , Email=$email";
    $old=$_POST["old"];
    $sex=$_POST["Sex"];
    $S=$_POST["S"];
    
    if($old <= "15"&& $sex =="ชาย"){
        $n=("ดช.");
    }elseif($old >"15"&& $sex =="ชาย"){
        $n=("นาย.");
    }elseif($old <="15"&& $sex =="หญิง"){
        $n=("ดญ.");
    }elseif($old >"15"&& $sex =="หญิง" && $S == "แต่งงานแล้ว"){
        $n=("นาง.");
    }elseif($old >"15"&& $sex =="หญิง"){
        $n=("นางสาว.");
    }
    echo"ชื่อ:$n$name สถานะ:$S อายุ:$old " ;    
    ?>

--- page1.php
+++ page1.php
@@ -5,8 +5,13 @@
 <form action="show.php" method="POST">
     Name:<input type="text" name="name"><br>
     Email:<input type="text"name="email"><br>
-    <input type="submit" value="send">
-</form>
+   ชื่อ:<input type="text" name="name"><br>
+    อายุ:<input type="var"name="old">
+    เพศ:<input type="text"name="Sex"><br>
+    สถานะ:
+    <input type="radio"name="S" value="แต่งงานแล้ว">แต่งงานแล้ว
+    <input type="radio"name="S" value="โสด">โสด
+    <br>
+    <input type="submit" value="send"> 
 </body>
-
 </html>
